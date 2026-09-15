3a2cc028f77db0e71b1bf2e7ee9115e67c12e503
PeetX <peetx2040@gmail.com>
2026-09-15 21:41:03 +0300

    14.NonfungiblePositionManager.sol

---
+++ 14.NonfungiblePositionManager.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "./PeetXAutomation.sol";
+
+abstract contract NonfungiblePositionManager is PeetXAutomation {
+    // Additional helper hooks or position management overrides can be placed here if needed.
+}
